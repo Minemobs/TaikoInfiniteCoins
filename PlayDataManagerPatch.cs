bd0506e23cbe410ef94eafa03d8eb12394ff85ea
minemobs <issahakim94@gmail.com>
2022-04-27 19:10:31 +0200

    Initial commit

---
+++ PlayDataManagerPatch.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+
+namespace TaikoInfiniteCoins
+{
+    [HarmonyPatch(typeof(PlayDataManager), nameof(PlayDataManager.GetDonCoin))]
+    public static class PlayDataManagerPatch
+    {
+        [HarmonyPrefix]
+        public static bool Prefix(ref int __result)
+        {
+            __result = DataConst.DonCoinMax;
+            return false;
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,34 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine.SceneManagement;
+
+namespace TaikoInfiniteCoins
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            Harmony harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+            harmony.PatchAll();
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+
+        private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            if(!scene.name.Equals("SongSelect")) return;
+            PlayDataManager playDataManager = FindObjectOfType<PlayDataManager>();
+            if (playDataManager == null) return;
+            Logger.LogInfo("PlayDataManager found!");
+            playDataManager.AddDonCoin(0, DataConst.DonCoinMax, true);
+            
+        }
+
+        private void OnDisable()
+        {
+            SceneManager.sceneLoaded -= OnSceneLoaded;
+        }
+    }
+}
